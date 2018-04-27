97c9c2c1d49ad0a19724a1afe730f4f4bb0c1e3d
Christian Castro <36249260+ChristianCastr0@users.noreply.github.com>
2018-04-26 22:38:45 -0700

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,55 +1,33 @@
-import java.util.HashMap;
+import java.sql.*;
 
-public class Login {
-	//variables for user's input
-	private String username = null;
-	private String password =  null;
-	//Indicates whether user is logged in or not
-	boolean status = false;
-	
-	//constructor
-	public void login(String username, String password, boolean status) {
-		this.username = username;
-		this.password = password;
-		this.status = status;
-	}
-	
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
-	
-	public boolean getStatus() {
-		return status;
-	}
-	
+public class Login{
 	//Check to see that what the user input exists and matches what's in the database
-	public String validate(HashMap<String, String> db, String username, String password) {
-		String update = null;
+	public static boolean validate(String un, String pw) {
+		boolean status=false;
 		
-		if(db.containsKey(username) && password.equals(db.get(username))) {
-			status = true;
-			update = "You have successfully logged in!";
-		}
-		
-		else
-			update = "Username or Password was incorrect!";
-		
-		return update;
-	}
+		//Establish connection to MySQL
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection(  
+					"jdbc:mysql://localhost:3306/moviedb","root","");  
+ 
+			Statement stmt=con.createStatement();
+			
+			//execute query for password for given username
+			ResultSet rs=stmt.executeQuery("SELECT password FROM users WHERE username='"+un+"'");  
+			rs.beforeFirst();
+			rs.next();
+			
+			//if entered password matches password in database return true
+			if(pw.equals(rs.getString(1))) {
+				status = true;
+			}
+			//close connection
+			con.close();
+		} 
+
+		catch(Exception e){ 
+			System.out.println(e);}  
+		return status;
+	}  
 }
