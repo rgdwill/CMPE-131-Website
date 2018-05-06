113fce3a6e97afed12437a661fbe02a43215a683
ricky4918 <ricky4918@gmail.com>
2018-05-06 15:34:14 +0900

    Update Search.java

--- Search.java
+++ Search.java
@@ -13,7 +13,8 @@ public class Search{
 					"jdbc:mysql://localhost:3306/moviedb","root","");  
  
 			Statement st=con.createStatement();
-	rs = st.executeQuery("SELECT *" + " FROM our database" +" WHERE movieName LIKE"+ '"'+"%"+query+"%"+'"'); 
+	rs = st.executeQuery("SELECT *" + " FROM our database" +" WHERE title = '"+userinput+"', 
+			     genre='"+userinput+"', year='"+userinput+"', id='"+userinput+"'); 
 
 // Here  I have to search year, genre, and id , but I could not come up with the method....//
 while(rs.next()) {
